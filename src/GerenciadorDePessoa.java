1f14bb664a060a97c42bd22eb8dc4c526558dc5b
Luiz Gabriel Bonotto <luizgabrielbonotto@hotmail.com>
2024-03-12 20:20:15 -0300

    first edit Class GerenciadorDePessoa

--- src/GerenciadorDePessoa.java
+++ src/GerenciadorDePessoa.java
@@ -1,2 +1,46 @@
+import pessoa.Pessoa;
+import pessoa.PessoaFisica;
+import pessoa.PessoaJuridica;
+import veiculo.Veiculo;
+
+import java.security.spec.PSSParameterSpec;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenciadorDePessoa {
+    private List<Pessoa> pessoas = new ArrayList<>();
+
+    public void cadastrarPessoa(String nome, String documento) {
+        if (this.pessoaExiste(documento) != null) {
+            return;
+        }
+        // verificar o tipo de pessoa pelo documento informado:
+        if (documento.length() > 11) {
+            Pessoa pessoaNova = new PessoaJuridica(nome, documento);
+            this.pessoas.add(pessoaNova);
+        } else {
+            Pessoa pessoaNova = new PessoaFisica(nome, documento);
+            this.pessoas.add(pessoaNova);
+        }
+
+    }
+
+    public void listarPessoas() {
+        if (this.pessoas.isEmpty()) {
+            System.out.println("Ainda não há pessoas cadastradas.");
+            return;
+        }
+
+        System.out.println("|    Nome     |     Documento   |");
+        System.out.println("| ----------- | --------------- |");
+
+        for (Pessoa pessoa : this.pessoas) {
+            System.out.println("| " + String.format("%11s", pessoa.getDocumento()) + " | " + String.format("%15s", pessoa.getNome()) + " |");
+        }
+    }
+
+    private Pessoa pessoaExiste(String documento) {
+        return this.pessoas.stream().filter(pessoa -> pessoa.getDocumento().equalsIgnoreCase(documento)).findFirst().orElse(null);
+    }
+
 }
