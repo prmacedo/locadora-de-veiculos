153508d6637b4d5cee321e14423ac440a9c2abd9
Paulo Macêdo <44329969+prmacedo@users.noreply.github.com>
2024-03-11 21:41:35 -0300

    create Veiculo functions

--- src/SistemaLocadora.java
+++ src/SistemaLocadora.java
@@ -1,6 +1,5 @@
 import pessoa.Pessoa;
 import registro.Registro;
-import veiculo.Veiculo;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,9 +8,8 @@ import java.util.ArrayList;
 public class SistemaLocadora {
     private List<Pessoa> clientes = new ArrayList<>();
     private List<Registro> historico = new ArrayList<>();
-    private List<Veiculo> veiculos = new ArrayList<>();
 
-    public <T extends Pessoa> void cadastrarCliente(String nome, String documento) {}
+    public void cadastrarCliente(String nome, String documento) {}
 
     public void alterarCliente(String documento, String nomeNovo) {}
 
