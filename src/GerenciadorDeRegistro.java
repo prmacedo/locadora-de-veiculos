78d7aa588508bd2cca5cf492c9f3e4d0a1161179
Paulo Macêdo <44329969+prmacedo@users.noreply.github.com>
2024-03-13 20:38:10 -0300

    create method to calculate number of daily rates

--- src/GerenciadorDeRegistro.java
+++ src/GerenciadorDeRegistro.java
@@ -1,4 +1,16 @@
+import registro.Aluguel;
+import registro.Devolucao;
+
+import java.time.temporal.ChronoUnit;
+
 public class GerenciadorDeRegistro {
 
+    private int calcularDiarias(Aluguel aluguel, Devolucao devolucao) {
+        long diferencaEmMinutos = aluguel.getDataHora().until(devolucao.getDataHora(), ChronoUnit.MINUTES);
+        double diferencaEmDias = diferencaEmMinutos / 1440d;
+        double diasArredondados = Math.ceil(diferencaEmDias);
+
+        return (int) diasArredondados;
+    }
 
 }
