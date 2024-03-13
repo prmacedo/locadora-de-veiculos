46cff84c0f23d441a06c8e8cb133636f89f09861
Luiz Gabriel Bonotto <luizgabrielbonotto@hotmail.com>
2024-03-13 20:42:35 -0300

    add cadastrarPessoas, removerCaracteres

--- src/GerenciadorDePessoa.java
+++ src/GerenciadorDePessoa.java
@@ -11,6 +11,9 @@ public class GerenciadorDePessoa {
         if (this.pessoaExiste(documento) != null) {
             return;
         }
+        //testar retorno da função
+        documento = removerCaracteres(documento);
+
         // verificar o tipo de pessoa pelo documento informado:
         if (documento.length() > 12) {
             Pessoa pessoaNova = new PessoaJuridica(nome, documento);
@@ -18,6 +21,20 @@ public class GerenciadorDePessoa {
         } else {
             Pessoa pessoaNova = new PessoaFisica(nome, documento);
             this.pessoas.add(pessoaNova);
+        }
+    }
+
+    public void alterarPessoa(String documentoAntigo, String documentoNovo, String nomeNovo) {
+        Pessoa documentoBuscado = this.pessoaExiste(documentoAntigo);
+        if (documentoBuscado == null) {
+            return;
+        }
+        documentoNovo = removerCaracteres(documentoNovo);
+        if (!documentoNovo.trim().isEmpty()) {
+            documentoBuscado.setDocumento(documentoNovo);
+        }
+        if (!nomeNovo.trim().isEmpty()) {
+            documentoBuscado.setNome(nomeNovo);
         }
     }
     public Pessoa buscarPessoa(String termoDeBusca) {
@@ -42,6 +59,10 @@ public class GerenciadorDePessoa {
 
     private Pessoa pessoaExiste(String documento) {
         return this.pessoas.stream().filter(pessoa -> pessoa.getDocumento().equalsIgnoreCase(documento)).findFirst().orElse(null);
+    }
+
+    private String removerCaracteres (String documento) {
+        return documento.replaceAll("[^0-9]", "");
     }
 
 }
