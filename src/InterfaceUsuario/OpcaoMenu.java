4a500800777458bb9bbb669966060e6b73973f65
Ricard Roberg <ricardroberg@gmail.com>
2024-03-13 21:40:15 -0300

    alterando enum

--- src/InterfaceUsuario/OpcaoMenu.java
+++ src/InterfaceUsuario/OpcaoMenu.java
@@ -1,16 +1,19 @@
 package InterfaceUsuario;
 
-import pessoa.Pessoa;
-import veiculo.Veiculo;
+import GerenciadorDePessoa;
+import GerenciadorDeVeiculo;
 
 public enum OpcaoMenu {
-    CADASTRAR_VEICULOS(Veiculo::cadastrarVeiculos),
-    ALTERAR_VEICULO(Veiculo::alterarVeiculo),
-    BUSCAR_VEICULO(Veiculo::buscarVeiculo),
-    CADASTRAR_CLIENTE(Pessoa::cadastrarCliente),
-    ALTERAR_CLIENTE(Pessoa::alterarCliente),
-    ALUGAR_VEICULO(Veiculo::alugarVeiculo),
-    DEVOLVER_VEICULO(Veiculo::devolverVeiculo),
+    CADASTRAR_VEICULOS(() -> GerenciadorDeVeiculo.cadastrarVeiculo()),
+    ALTERAR_VEICULO(() -> GerenciadorDeVeiculo.alterarVeiculo()),
+    BUSCAR_VEICULO(() -> GerenciadorDeVeiculo.buscarVeiculo()),
+    LISTAR_VEICULOS(() -> GerenciadorDeVeiculo.listarVeiculos()),
+    ALUGAR_VEICULO(() -> GerenciadorDeVeiculo.alugarVeiculo()),
+    DEVOLVER_VEICULO(() -> GerenciadorDeVeiculo.devolverVeiculo()),
+    CADASTRAR_CLIENTE(() -> GerenciadorDePessoa.cadastrarPessoa()),
+    BUSCAR_CLIENTE(() -> GerenciadorDePessoa.buscarPessoa()),
+    LISTAR_CLIENTES(() -> GerenciadorDePessoa.listarPessoas()),
+    ALTERAR_CLIENTE(() -> GerenciadorDePessoa.alterarCliente()),
     SAIR(Menu::sair);
 
     private final Runnable acao;
