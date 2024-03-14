4e0a9597faa59467d33531ae6d3b26cbbe4befa1
rafaelluis1 <rafael.luis.carvalho.maia@ccc.ufcg.edu.br>
2024-03-13 21:04:11 -0300

    implement GerenciadorDeRegistro

--- src/registro/Registro.java
+++ src/registro/Registro.java
@@ -12,10 +12,11 @@ public abstract class Registro {
 
     private Veiculo veiculo;
 
-    public Registro(String local, LocalDateTime dataHora, Pessoa cliente) {
+    public Registro(String local, LocalDateTime dataHora, Pessoa cliente, Veiculo veiculo) {
         this.local = local;
         this.dataHora = dataHora;
         this.cliente = cliente;
+        this.veiculo = veiculo;
     }
 
     public String getLocal() {
@@ -41,6 +42,16 @@ public abstract class Registro {
     public void setCliente(Pessoa cliente) {
         this.cliente = cliente;
     }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+
 }
 
 
