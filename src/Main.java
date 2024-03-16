45b0e23ebcd12a19f2839b4d68ff0ed6b1beeb39
Paulo Macêdo <44329969+prmacedo@users.noreply.github.com>
2024-03-16 11:48:38 -0300

    remove unused imports from Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import Gerenciadores.GerenciadorDePessoa;
-import Gerenciadores.GerenciadorDeVeiculo;
 import InterfaceUsuario.Menu;
-import veiculo.TipoVeiculo;
-
-import java.util.Scanner;
 
 public class Main extends Menu {
     public static void main(String[] args) throws InterruptedException {
